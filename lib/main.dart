74e5e6f438a4cc02471d1405f75867cb64af6528
Michael Rahardja <michaelrahardja23@gmail.com>
2024-09-09 22:14:15 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:uts_mobprog_kelompok_2/pages/root.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Root(),
+  ));
+}
---
+++ lib/pages/root.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatefulWidget {
+  const Root({super.key});
+
+  @override
+  State<Root> createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  int currPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FastDrive'),
+      ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currPageIndex = index;
+          });
+        },
+        selectedIndex: currPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.list), label: 'History'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+      body: <Widget>[
+        Text('Home'),
+        Text('History'),
+        Text('Profile'),
+      ][currPageIndex],
+    );
+  }
+}
