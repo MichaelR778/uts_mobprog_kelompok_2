5bc0890a84bfaf902af5fd7bb711d5b72e7bdd27
Michael Rahardja <michaelrahardja23@gmail.com>
2024-09-13 12:34:41 +0700

    test merge

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,7 @@ import 'package:uts_mobprog_kelompok_2/pages/root.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Utiwi',
-<<<<<<< HEAD
     theme: ThemeData(fontFamily: 'Handjet'),
-=======
->>>>>>> origin/dimas
     home: Root(),
   ));
 }
