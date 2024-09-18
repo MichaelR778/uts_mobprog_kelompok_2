273c84c44ef7f3aeb8200119e3639d7d67a3818e
william535230174 <william.535230174@stu.untar.ac.id>
2024-09-18 17:52:34 +0700

    change pin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:uts_mobprog_kelompok_2/pages/root.dart';
+import 'package:uts_mobprog_kelompok_2/pages/change_pin.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Root(),
   ));
+}
+
+class MyApp extends StatelessWidget { 
+  const MyApp({super.key}); 
+  
+  @override
+  Widget build(BuildContext context) { 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo', 
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), 
+        useMaterial3: true, 
+      ), 
+      home: const change_pin(), 
+    );
+  }
 }
